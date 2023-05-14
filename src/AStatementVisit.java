eb1796344f5716db7dbe584a42c48580e11ee2be
metazer0 <isaacdvid15.96@outlook.com>
2023-05-13 20:30:34 -0600

    added while statement visitor

--- src/AStatementVisit.java
+++ src/AStatementVisit.java
@@ -45,7 +45,13 @@ public class AStatementVisit extends MiniPascalBaseVisitor<AStatementNode>{
 
     @Override
     public AStatementNode visitWhileStatement(MiniPascalParser.WhileStatementContext ctx) {
-        return super.visitWhileStatement(ctx);
+        String whileKeyword = ctx.WHILE().getText();
+        AExpressionVisitor expressionVisitor = new AExpressionVisitor();
+        AExpressionNode expressionNode = expressionVisitor.visit(ctx.expression());
+        String doKeyword = ctx.DO().getText();
+        AStatementVisit statementVisitor = new AStatementVisit();
+        AStatementNode statementNode = statementVisitor.visit(ctx.statement());
+        return new AWhileStatementNode(whileKeyword,expressionNode,doKeyword,statementNode);
     }
 
     @Override

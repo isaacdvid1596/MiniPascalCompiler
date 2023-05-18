9d590ebff5ce8ec68ebe1a2bfeaf60bbdd430a28
metazer0 <isaacdvid15.96@outlook.com>
2023-05-17 19:05:46 -0600

    finished functionblock visitor and corrected codeblockvisitor

--- src/AFunctionBlockNode.java
+++ src/AFunctionBlockNode.java
@@ -2,17 +2,13 @@ import java.util.List;
 
 public class AFunctionBlockNode {
     private List<AVarDeclarationNode> variableDeclarations;
-    private List<ACompoundStatementNode> compoundStatementNode;
+    private ACompoundStatementNode compoundStatementNode;
     private String functionEndKeyword;
 
-    public AFunctionBlockNode(List<AVarDeclarationNode> variableDeclarations,List<ACompoundStatementNode> compoundStatementNode, String functionEndKeyword) {
+    public AFunctionBlockNode(List<AVarDeclarationNode> variableDeclarations,ACompoundStatementNode compoundStatementNode, String functionEndKeyword) {
         this.variableDeclarations = variableDeclarations;
         this.compoundStatementNode = compoundStatementNode;
         this.functionEndKeyword = functionEndKeyword;
     }
 
-    public AFunctionBlockNode(List<ACompoundStatementNode> compoundStatementNode, String functionEndKeyword) {
-        this.compoundStatementNode = compoundStatementNode;
-        this.functionEndKeyword = functionEndKeyword;
-    }
 }
